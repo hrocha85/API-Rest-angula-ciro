1e80cff5e09d0d802e0f5224f98afdb5aa55b9f4
hrocha85 <hrocha85@gmail.com>
2020-12-03 15:25:35 -0300

    commit
    Signed-off-by: hrocha85 <hrocha85@gmail.com>

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,13 +9,32 @@ import { ConsultService } from '../consult.service';
 })
 export class ProductsComponent implements OnInit {
 
+  nome:string;
+  description:string;
+  price:number;
+  categories:string;
+
+
   constructor(public consultService: ConsultService) {  }
 
   ngOnInit(): void {
 
     this.consultService.updateProdutos();
-
+    this.consultService.updateCategorias();
 
   }
+
+  criar(frm: FormGroup){
+
+    const info={
+      nome: this.nome,
+      description: this.description,
+      price:this.price,
+      categories: this.categories,
+    }
+
+    console.log(info);
+  }
+
 
 }
