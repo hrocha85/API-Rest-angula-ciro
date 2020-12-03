569410f655a5daed97af0d3350c9e3a3ac4a7249
Gustavo <gustavoluigib@gmail.com>
2020-12-03 17:51:49 -0300

    fix post categories

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -9,29 +9,38 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
-  id:string;
+  id: string;
   nome: string;
+  categories: Array<any>;
 
-  constructor(public consultService: ConsultService , private http: HttpClient)
- {}
+  constructor(
+    public consultService: ConsultService,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.consultService.updateCategorias();
 
-    this.http.post<any>('http://localhost:8080/categories',{ nome: this.nome,id: this.id}).subscribe(data =>{
-      this.nome = data.nome;
-
-    })
+    this.http
+      .post<any>('http://localhost:8080/categories', {
+        nome: this.nome,
+        id: this.id,
+      })
+      .subscribe((data) => {
+        this.nome = data.nome;
+      });
   }
 
   criar(frm: FormGroup) {
     const info = {
+      id: Math.floor(Math.random() * 100).toString(),
       nome: this.nome,
     };
 
     console.log(info);
 
-    this.consultService.pushCategorias();
-
+    this.consultService
+      .criarCategoria(info)
+      .subscribe((categorie) => this.categories.push(categorie));
   }
 }
--- src/app/consult.service.ts
+++ src/app/consult.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 interface Response {
   id: string;
@@ -70,6 +72,21 @@ export class ConsultService {
 
   products: Array<ResponseProducts> = [];
 
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError('Something bad happened; please try again later.');
+  }
+
   updateUser() {
     this.http
       .get<Array<Response>>('http://localhost:8080/users')
@@ -111,17 +128,12 @@ export class ConsultService {
 
   // }
 
-  // criarCategoria(nome: any) {
-
-  //   this.http.post<any>('http://localhost:8080/categories', nome)
-
-
-  // }
-
-  pushCategorias() {
-    // this.criarCategoria.subscribe((resposta) => {
-    //   // this.categoreis.push(respostas);
-    //   // frm.reset();
-    // });
+  criarCategoria(
+    categorie: ResponseCategories
+  ): Observable<ResponseCategories> {
+    return this.http.post<ResponseCategories>(
+      'http://localhost:8080/categories',
+      categorie
+    );
   }
 }
