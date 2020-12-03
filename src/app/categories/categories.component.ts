898044fef2b20d86ff76782ab92286cb25fd6dd0
Gustavo <gustavoluigib@gmail.com>
2020-12-03 13:54:13 -0300

    input categories

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -2,38 +2,25 @@ import { Component, OnInit, ɵConsole } from '@angular/core';
 import { ConsultService } from '../consult.service';
 import { FormGroup } from '@angular/forms';
 
-
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  nome: string;
 
-   nome:string;
-
-
-  constructor(public consultService: ConsultService ) { }
+  constructor(public consultService: ConsultService) {}
 
   ngOnInit(): void {
-
     this.consultService.updateCategorias();
-
   }
 
-  criar(frm: FormGroup){
-
-    const info={
-      nome: this.nome
-
+  criar(frm: FormGroup) {
+    const info = {
+      nome: this.nome,
     };
 
-    console.log(this.nome);
-
-
+    console.log(info);
   }
-
-
-
-
 }
--- src/app/consult.service.ts
+++ src/app/consult.service.ts
@@ -1,159 +1,124 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-
-interface Response{
-
-
-    id:string;
-    nome:string;
-    email:string;
-    telefone: number;
-
-
-}
-
-interface ResponseCategories{
+interface Response {
   id: string;
   nome: string;
+  email: string;
+  telefone: number;
+}
 
+interface ResponseCategories {
+  id: string;
+  nome: string;
 }
 
 interface ResponseOrders {
-
-
-    id:string;
-    moment:string;
-    orderStatus: string;
-    client:{
-      id:string;
-      nome:string;
-      email:string;
-      telefone:number;
-    }
-
-  ,
-  items:{
-    quantity:number;
-    price:number;
-    subtotal:number;
-  product:{
-    id:string;
-    name:string;
-    description:string;
-    price:number;
-    imgUrl:string;
-    categories:{
-      id:string;
-      nome:string;
-    }
-  },
-  payment: boolean;
-  total:number;
-  }
-
-};
-
-interface ResponseProducts{
-
+  id: string;
+  moment: string;
+  orderStatus: string;
+  client: {
     id: string;
-    name: string;
-    description: string;
+    nome: string;
+    email: string;
+    telefone: number;
+  };
+  items: {
+    quantity: number;
     price: number;
-    imgUrl: string;
-    categories:{
+    subtotal: number;
+    product: {
+      id: string;
+      name: string;
+      description: string;
+      price: number;
+      imgUrl: string;
+      categories: {
         id: string;
-        nome:string;
+        nome: string;
       };
-
     };
+    payment: boolean;
+    total: number;
+  };
+}
 
-
+interface ResponseProducts {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  imgUrl: string;
+  categories: {
+    id: string;
+    nome: string;
+  };
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConsultService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  user: Array <Response> = [ ];
+  user: Array<Response> = [];
   lastUpdate: Date;
 
-  categories:Array<ResponseCategories> = [];
+  categories: Array<ResponseCategories> = [];
 
-  orders:Array<ResponseOrders> = [];
+  orders: Array<ResponseOrders> = [];
 
-  products:Array<ResponseProducts> = [];
+  products: Array<ResponseProducts> = [];
 
+  updateUser() {
+    this.http
+      .get<Array<Response>>('http://localhost:8080/users')
+      .subscribe((data) => {
+        this.user = data;
+        this.lastUpdate;
+      });
+  }
 
-  updateUser(){
+  updateCategorias() {
+    this.http
+      .get<Array<ResponseCategories>>('http://localhost:8080/categories')
+      .subscribe((data) => {
+        this.categories = data;
+        this.lastUpdate;
+      });
+  }
 
-  this.http.get<Array<Response>>('http://localhost:8080/users').subscribe(data =>{
-    this.user = data;
-    this.lastUpdate;
-    });
+  updatePedidos() {
+    this.http
+      .get<Array<ResponseOrders>>('http://localhost:8080/orders')
+      .subscribe((data) => {
+        this.orders = data;
+        this.lastUpdate;
+      });
+  }
 
-    }
+  updateProdutos() {
+    this.http
+      .get<Array<ResponseProducts>>('http://localhost:8080/products')
+      .subscribe((data) => {
+        this.products = data;
+        this.lastUpdate;
+      });
+  }
 
-  updateCategorias(){
+  // criar(contato: any){
+  //   return this.http.post('http://localhost:8080/users', contato );
 
-  this.http.get<Array<ResponseCategories>>('http://localhost:8080/categories').subscribe(data =>{
-    this.categories = data;
-    this.lastUpdate;
-    });
+  // }
 
-    }
+  criarCategoria(nome: any) {
+    return this.http.post('http://localhost:8080/categories', nome);
+  }
 
-  updatePedidos(){
-
-  this.http.get<Array<ResponseOrders>>('http://localhost:8080/orders').subscribe(data =>{
-    this.orders = data;
-    this.lastUpdate;
-    });
-
-    }
-
-  updateProdutos(){
-
-  this.http.get<Array<ResponseProducts>>('http://localhost:8080/products').subscribe(data =>{
-    this.products = data;
-    this.lastUpdate;
-    });
-
-    }
-
-
-
-
-
-    // criar(contato: any){
-    //   return this.http.post('http://localhost:8080/users', contato );
-
-    // }
-
-    criarCategoria(nome: any){
-      return this.http.post('http://localhost:8080/categories', nome );
-
-    }
-
-   pushCategorias(){
-
-    this.criarCategoria.subscribe(resposta => {
-      this.categoreis.push(respostas);
-
-
-      frm.reset();
-    })
-
-   }
-
-
-
-
-
-
-
-
-
+  pushCategorias() {
+    // this.criarCategoria.subscribe((resposta) => {
+    //   // this.categoreis.push(respostas);
+    //   // frm.reset();
+    // });
+  }
 }
